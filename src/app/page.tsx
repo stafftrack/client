14d2fe869579dd905f09b02bc854c0bcc7b06a07
justinli <kobs666666@gmail.com>
2023-10-21 12:01:28 +0800

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ import { createClient } from '@supabase/supabase-js';
 import SearchIcon from '@/components/Fiter/SearchIcon';
 import { DataRow } from '@/types';
 
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
